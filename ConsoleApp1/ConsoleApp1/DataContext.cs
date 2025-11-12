efa2960b4c3c8a1e619a8b16248d426377925e40
Miraziz <130227403+MMiraziz013@users.noreply.github.com>
2025-11-12 15:56:34 +0500

    Added All features.

---
+++ ConsoleApp1/ConsoleApp1/DataContext.cs
@@ -0,0 +1,35 @@
+namespace ConsoleApp1;
+
+public class DataContext : IdentityDbContext<User, IdentityRole<int>, int>, IDataContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<SalaryAnomaly> SalaryAnomalies { get; set; }
+    public DbSet<SalaryHistory> SalaryHistories { get; set; }
+    public DbSet<PayrollRecord> PayrollRecords { get; set; }
+    public DbSet<VacationBalance> VacationBalances { get; set; }
+    public DbSet<VacationRecord> VacationRecords { get; set; }
+
+    public async Task MigrateAsync()    
+    {
+        await Database.MigrateAsync();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(UserConfigurations).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(EmployeeConfigurations).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(DepartmentConfigurations).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(VacationBalanceConfigurations).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(VacationRecordConfigurations).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(SalaryAnomalyConfigurations).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(SalaryHistoryConfigurations).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(PayrollRecordConfigurations).Assembly);
+    } 
+}
